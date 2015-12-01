d0053e59a848142142aa64c08a9708f2a45367de
Chris Didyk <cdidyk@gmail.com>
2015-12-01 13:51:00 -0500

    Fix default host on production for url helpers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,18 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "shaolinstpete.com" }
+
+  config.action_mailer.default_url_options = {
+    host: "kwoon.shaolinstpete.com",
+    protocol: "https://"
+  }
   config.action_mailer.smtp_settings = {
     openssl_verify_mode: "none"
+  }
+
+  config.action_controller.default_url_options = {
+    host: "kwoon.shaolinstpete.com",
+    protocol: "https://"
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
